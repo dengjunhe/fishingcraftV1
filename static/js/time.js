7d2ab81a7179798d6b92fbd6f2169ba83777220d
Deng J.H <1559149202@qq.com>
2022-06-12 11:17:47 +0800

    Add files via upload

--- static/js/time.js
+++ static/js/time.js
@@ -12,7 +12,7 @@ function time() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    var t1 = Date.UTC(2022, 3, 13, 00, 00, 00)
+    var t1 = Date.UTC(2022, 1, 30, 00, 00, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
